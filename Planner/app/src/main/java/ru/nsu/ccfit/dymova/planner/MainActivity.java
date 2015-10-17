8d275ad3b99b790b7775aa045900e2aa1105e29c
Dymova Anastasiya <adimova05@gmail.com>
2015-10-17 15:25:16 +0600

    add intet for go to Task description activity and come back

--- Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/MainActivity.java
+++ Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/MainActivity.java
@@ -5,23 +5,41 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private FloatingActionButton fab;
+    private ListView tasksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         initToolbar();
         initFloatingActionButton();
+        fillingTasksList();
 
+    }
+
+    private void fillingTasksList() {
+        tasksList = (ListView) findViewById(R.id.tasks_list);
+//        todo
+        String[] items = new String[20];
+        for (int i = 0; i < 20; i++) {
+            items[i] = "intem [" + i + "]";
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        tasksList.setAdapter(adapter);
     }
 
     private void initFloatingActionButton() {
@@ -40,15 +58,18 @@ public class MainActivity extends Activity{
     }
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
-        toolbar.setTitleTextColor(Color.WHITE);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                return false;
-            }
-        });
-        toolbar.inflateMenu(R.menu.menu);
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            toolbar.setTitle(R.string.app_name);
+            toolbar.setTitleTextColor(Color.WHITE);
+            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return false;
+                }
+            });
+            toolbar.inflateMenu(R.menu.menu);
+        }
     }
 
 
--- Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/TaskDescriptionActivity.java
+++ Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/TaskDescriptionActivity.java
@@ -1,11 +1,15 @@
 package ru.nsu.ccfit.dymova.planner;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
-public class TaskDescriptionActivity extends Activity{
+public class TaskDescriptionActivity extends AppCompatActivity{
     private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +23,26 @@ public class TaskDescriptionActivity extends Activity{
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.add_new_task);
         toolbar.setTitleTextColor(Color.WHITE);
-//        if(toolbar != null) {
-//            setSupportActionBar(toolbar);
-//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        }
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //todo save to sqlite
+//                NavUtils.navigateUpFromSameTask(this);
+                goToHome();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void goToHome() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
