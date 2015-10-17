3126aad09099742d5bcd67650eacdcb5d53a772d
Dymova Anastasiya <adimova05@gmail.com>
2015-10-17 19:35:54 +0600

    change task decription activity for add new task

---
+++ Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/DbHelper.java
@@ -0,0 +1,4 @@
+package ru.nsu.ccfit.dymova.planner;
+
+public class DbHelper {
+}
--- Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/MainActivity.java
+++ Planner/app/src/main/java/ru/nsu/ccfit/dymova/planner/MainActivity.java
@@ -1,11 +1,9 @@
 package ru.nsu.ccfit.dymova.planner;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
